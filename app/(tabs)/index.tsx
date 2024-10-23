647d631d38c8907cde2280565e69268c95ed42aa
Esra Tansuk <esra.balkis@outlook.com>
2024-10-23 11:11:13 +0300

    :art: Navigator eklendi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,18 +2,29 @@ import { CategoriesScreen } from "@/screens/CategoriesScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
-        <CategoriesScreen />
+      <NavigationContainer independent={true}>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: "#fff",
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
